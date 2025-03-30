9c9eee7f8efce6c4923f9d5f04897e0e84057e5a
j-miet <191247923+j-miet@users.noreply.github.com>
2025-03-30 14:35:38 +0300

    Reduced plan file imports into one line
    Created a separate _plan_imports.py file inside plans folder which
    does all previous imports. Plan files need only to import this module.

---
+++ btd6bot/plans/_plan_imports.py
@@ -0,0 +1,13 @@
+"""All required imports for plan files."""
+
+from time import time
+
+from bot.commands.flow import begin, end_round, change_autostart, wait
+from bot.commands.monkey import Monkey
+from bot.commands.hero import Hero
+from bot.commands.ability import ability, click
+import bot.menu_start as menu_start
+from bot.rounds import Rounds
+
+__all__ = ['time', 'begin', 'end_round', 'change_autostart', 'wait', 'Monkey', 'Hero',
+           'ability', 'click', 'menu_start', 'Rounds']
