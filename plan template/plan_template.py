1028ea1c89e4c509d51a63137544c49b51f9fd14
j-miet <191247923+j-miet@users.noreply.github.com>
2025-04-20 18:56:17 +0300

    Remove extra imports

--- plan template/plan_template.py
+++ plan template/plan_template.py
@@ -13,13 +13,6 @@ _______________________________________
 '''
 from._plan_imports import *
 
-from bot.commands.flow import begin, end_round, change_autostart, wait
-from bot.commands.monkey import Monkey
-from bot.commands.hero import Hero
-from bot.commands.ability import ability, click
-import bot.menu_start as menu_start
-from bot.rounds import Rounds
-
 def play(rounds: tuple[str, str, str, int, int, str]) -> None:
     BEGIN, END = menu_start.load(*rounds)
     current_round = BEGIN-1
