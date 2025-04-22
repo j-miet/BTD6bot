7c97a92e4feac5001644032f00ed942d623249b9
j-miet <191247923+j-miet@users.noreply.github.com>
2025-04-22 15:57:41 +0300

    Add support for float values

--- btd6bot/utils/timing.py
+++ btd6bot/utils/timing.py
@@ -2,12 +2,17 @@
 
 import time
 
-def counter(seconds : int) -> None:
+def counter(seconds : int | float) -> None:
     """Counts down to 1, starting from number 'seconds'.
 
     Args:
-        seconds: Start value as seconds, a non-negative integer.
+        seconds: Start value as seconds. If value in int, counts the time down; if float value, no counting is done and 
+            only total time is displayed.
     """
-    for t in range(seconds, 0, -1):
-        print(str(t), end=' ')
-        time.sleep(1)   
+    if isinstance(seconds, int):
+        for t in range(seconds, 0, -1):
+            print(str(t), end=' ')
+            time.sleep(1)
+    elif isinstance(seconds, float):
+        print(seconds)
+        time.sleep(seconds)
