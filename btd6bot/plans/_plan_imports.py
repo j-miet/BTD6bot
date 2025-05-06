18fec541cd5ce69e707ef2a4390671e49e492f8c
j-miet <191247923+j-miet@users.noreply.github.com>
2025-05-06 23:10:05 +0300

    Add support for move_cursor

--- btd6bot/plans/_plan_imports.py
+++ btd6bot/plans/_plan_imports.py
@@ -2,7 +2,7 @@
 
 from time import time
 
-from bot.commands.ability import ability, click
+from bot.commands.ability import ability, click, move_cursor
 from bot.commands.flow import forward, end_round, change_autostart, wait
 from bot.commands.monkey import Monkey
 from bot.commands.hero import Hero
@@ -10,4 +10,4 @@ import bot.menu_start as menu_start
 from bot.rounds import Rounds
 
 __all__ = ['time', 'menu_start', 'Rounds', 'Monkey', 'Hero',
-            'ability', 'click', 'forward', 'end_round', 'change_autostart', 'wait']
+            'ability', 'click', 'forward', 'end_round', 'change_autostart', 'wait', 'move_cursor']
