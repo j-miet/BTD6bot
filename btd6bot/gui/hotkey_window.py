be89d05cb79c479d4e02b6d01cd8219412cbc308
j-miet <191247923+j-miet@users.noreply.github.com>
2025-05-28 18:35:05 +0300

    Update text font, add macOS support
    Because setting hotkeys uses pynput.Listener, it had to reworked for
    macOS. On this operating system, user must manually write the key to a
    popup window. For modifier keys (which are listed in hotkeys info), the
    'Key.' must be added in front e.g. Key.ctrl.
    
    Already existing system is still used for non-mac os.

--- btd6bot/gui/hotkey_window.py
+++ btd6bot/gui/hotkey_window.py
@@ -2,11 +2,14 @@
 
 from __future__ import annotations
 from typing import TYPE_CHECKING
+import sys
 import tkinter as tk
+from tkinter import simpledialog
 
 import pynput
 
 import gui.gui_paths as gui_paths
+from gui.gui_tools import os_font
 from utils import plan_data
 
 if TYPE_CHECKING:
@@ -49,25 +52,75 @@ class HotkeyWindow:
         self.hotkeywindow.minsize(800,600)
         self.hotkeywindow.maxsize(800,600)
 
-        hotkeylabel = tk.Label(self.hotkeywindow, text='Hotkeys', height=1, relief="groove")
+        hotkeylabel = tk.Label(self.hotkeywindow, text='Hotkeys', height=1, relief="groove", font=os_font)
         hotkeylabel.grid(column=0, row=0, sticky="nsew", padx=20, pady=5)
-        self.hotkeyoptionlist = tk.Listbox(self.hotkeywindow, width=30)
+        self.hotkeyoptionlist = tk.Listbox(self.hotkeywindow, width=30, font=os_font)
         self.hotkeyoptionlist.grid(column=0, row=1, padx=20, sticky="nsew")
 
-        self.guihotkeyoptionlist = tk.Listbox(self.hotkeywindow, width=30, height=3)
+        self.guihotkeyoptionlist = tk.Listbox(self.hotkeywindow, width=30, height=3, font=os_font)
         self.guihotkeyoptionlist.grid(column=0, row=2, padx=20, sticky="nsew")
 
-        self.sethotkeybutton = tk.Button(self.hotkeywindow, text='Set hotkey', width=15, height=1,
+        self.sethotkeybutton = tk.Button(self.hotkeywindow, text='Set hotkey', width=15, height=1, font=os_font,
                                          command=self.set_hotkey)
         self.sethotkeybutton.grid(column=0, row=3, sticky="s")
 
-        hotkeyhelplabel = tk.Label(self.hotkeywindow, text='Instructions', height=1, relief="groove")
+        hotkeyhelplabel = tk.Label(self.hotkeywindow, text='Instructions', height=1, relief="groove", font=os_font)
         hotkeyhelplabel.grid(column=1, row=0, sticky="nsew", padx=20, pady=5)
-        self.info = tk.Label(self.hotkeywindow, justify='left', relief='sunken', text=self.showhotkeyhelp())
+        self.info = tk.Label(self.hotkeywindow, justify='left', relief='sunken', text=self.showhotkeyhelp(), 
+                             font=os_font)
         self.info.grid(column=1, row=1, padx=20, sticky="nsew")
 
         self.read_hotkeys()
         self.read_guihotkeys()
+
+        if sys.platform == 'darwin':
+            self.guihotkeyoptionlist['state'] = 'disabled'
+            self.info.config(text=self.info.cget("text")[:-186]+"\n[MacOS]\n-gui hotkeys don't currently work on mac " 
+                             "systems and are disabled.")
+            self.sethotkeybutton = tk.Button(self.hotkeywindow, text='Edit selected entry', width=15, height=1, 
+                                            font=os_font,
+                                            command=self.save_hotkeys)
+            self.sethotkeybutton.grid(column=0, row=3, sticky="s")
+
+    def save_hotkeys(self) -> None:
+        """Set a hotkey value for currently highlighted row without pynput.Listener.
+        
+        Similar to set_hotkey, but requires user to manually type in hotkeys. 
+        
+        Used with mac operating systems as pynput causes critical errors with listener objects for some unknown reason.
+        """
+        self.sethotkeybutton.configure(state='disabled')
+        new_input = self.hotkeyoptionlist.curselection() # type: ignore
+        new_input2 = self.guihotkeyoptionlist.curselection() # type: ignore
+        if new_input == () and new_input2 == ():
+            self.sethotkeybutton.configure(state='active')
+            return
+        elif new_input != ():
+            selected = self.hotkeyoptionlist.curselection()[0] # type: ignore
+            selected_text = HotkeyWindow.hotkey_list[selected].split(' = ')[0]
+            user_input = simpledialog.askstring(title="Hotkey", prompt="Insert a new key value for: "+selected_text)
+            if user_input is not None:
+                if len(user_input) != 0:
+                    HotkeyWindow.hotkey_list[selected] = selected_text + ' = ' + user_input
+                    with open(gui_paths.HOTKEYS_PATH, 'w') as file_write:
+                        for line in HotkeyWindow.hotkey_list:
+                            file_write.write(line+'\n')
+                    self.hotkeyoptionlist.delete(0, 'end')
+                    self.read_hotkeys()
+            self.sethotkeybutton.configure(state='active')
+        elif new_input2 != ():
+            selected = self.guihotkeyoptionlist.curselection()[0] # type: ignore
+            selected_text = HotkeyWindow.guihotkey_list[selected].split(' = ')[0]
+            user_input = simpledialog.askstring(title="Hotkey", prompt="Insert a new key value for: "+selected_text)
+            if user_input is not None:
+                if len(user_input) != 0:
+                    HotkeyWindow.guihotkey_list[selected] = selected_text + ' = ' + user_input
+                    with open(gui_paths.GUIHOTKEYS_PATH, 'w') as file_write:
+                        for line in HotkeyWindow.guihotkey_list:
+                            file_write.write(line+'\n')
+                    self.guihotkeyoptionlist.delete(0, 'end')
+                    self.read_guihotkeys()
+            self.sethotkeybutton.configure(state='active')
 
     def showhotkeyhelp(self) -> str:
         """Return hotkey help text for info label.
