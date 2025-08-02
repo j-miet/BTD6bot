0e14c8154ab2c4d04bcf973db596035ca315e696
j-miet <191247923+j-miet@users.noreply.github.com>
2025-08-02 19:23:26 +0300

    Add support for custom icon

--- btd6bot/__main__.py
+++ btd6bot/__main__.py
@@ -1,5 +1,6 @@
 """Welcome to BTD6bot!"""
 
+import pathlib
 import sys
 import tkinter as tk
 
@@ -12,12 +13,12 @@ def main(argv: list[str]) -> None:
     Args:
         argv (list[str]): Command line argument list. Only supported custom arg is '-nogui' which runs BTD6bot in 
             gui-free mode.
-
     """
     if len(argv) > 1 and argv[1] == '-nogui':
         _no_gui.run()
     else:
         root = tk.Tk()
+        root.iconbitmap(pathlib.Path(__file__).parent/'Files'/"btd6bot.ico")
         MainWindow(root)
         root.mainloop()
 
