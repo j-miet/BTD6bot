71ef60b66e0eb4d23e3e685e76c691e3a127f842
busy-duck <144853664+busy-duck@users.noreply.github.com>
2025-08-02 18:28:57 +0200

    rounds correction and add py extension to spring_springHardChimps

--- btd6bot/plans/spring_springHardChimps.py
+++ btd6bot/plans/spring_springHardChimps.py
@@ -25,21 +25,24 @@ def play(rounds: tuple[str, str, str, int, int, str]) -> None:
         if current_round == BEGIN:
             sub1 = Monkey('sub', 0.7510416667, 0.4601851852)
             dart1 = Monkey('dart', 0.1442708333, 0.5277777778)
-        elif current_round == 7:
+        elif current_round == 8:
             boat1 = Monkey('boat', 0.7197916667, 0.5842592593)
-        elif current_round == 10:
+        elif current_round == 11:
             hero = Hero(0.6479166667, 0.5407407407)
+            hero.target('strong')
         elif current_round == 12:
             boat1.upgrade(['1-0-0'])
+        elif current_round == 14:
+            boat1.upgrade(['2-0-0'])
         elif current_round == 15:
-            boat1.upgrade(['2-0-0','2-0-1'])
-        elif current_round == 18:
+            boat1.upgrade(['2-0-1'])
+        elif current_round == 17:
             sub1.upgrade(['0-0-1'])
         elif current_round == 21:
             sub1.upgrade(['0-0-2'])
         elif current_round == 23:
             boat1.upgrade(['2-0-2'])
-        elif current_round == 31:
+        elif current_round == 32:
             boat1.upgrade(['3-0-2'])
         elif current_round == 34:
             sub1.upgrade(['0-0-3'])
@@ -66,17 +69,16 @@ def play(rounds: tuple[str, str, str, int, int, str]) -> None:
             village1.upgrade(['4-2-0'])
             bomb1 = Monkey('bomb', 0.7192708333, 0.4500000000)
             bomb1.upgrade(['1-0-0','2-0-0','2-1-0','2-2-0'])            
-        elif current_round == 65:
+        elif current_round == 64:
             bomb1.upgrade(['3-2-0'])
-        elif current_round == 67:
+        elif current_round == 65:
             bomb1.upgrade(['4-2-0'])
-        elif current_round == 87:
+        elif current_round == 86:
             bomb1.upgrade(['5-2-0'])
             bomb2 = Monkey('bomb', 0.6750000000, 0.4981481481)
-            bomb2.upgrade(['0-0-1','0-0-2'])
         elif current_round == 87:
-            bomb2.upgrade(['1-0-2','2-0-2','2-0-3'])
+            bomb2.upgrade(['0-0-1','0-0-2','1-0-2','2-0-2'])
         elif current_round == 89:
-            bomb2.upgrade(['2-0-4'])
-        elif current_round == 98:
+            bomb2.upgrade(['2-0-3','2-0-4'])
+        elif current_round == 95:
             bomb2.upgrade(['2-0-5'])
