4ff1c0f66166f77aef57b45feb2ffb0bdd3743ca
j-miet <191247923+j-miet@users.noreply.github.com>
2025-08-20 15:54:21 +0300

    Remove rmtree command

--- scripts/update_helpwindow_imgs.py
+++ scripts/update_helpwindow_imgs.py
@@ -1,12 +1,9 @@
 """Auto-updates helpwindow images."""
 
-import os
 from pathlib import Path
 import shutil
 
 img_address = Path(__file__).parent.parent/'docs/images'
 destination = Path(__file__).parent.parent/'btd6bot/Files/helpwindow/images'
 
-if os.path.isdir(destination):
-    shutil.rmtree(destination)
-shutil.copytree(img_address, destination)
+shutil.copytree(img_address, destination, dirs_exist_ok=True)
