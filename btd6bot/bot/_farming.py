0e056ec3c54b52b765747f9747aeff9732e9183c
j-miet <191247923+j-miet@users.noreply.github.com>
2025-10-20 16:58:55 +0300

    Set higher failurelimit value

--- btd6bot/bot/_farming.py
+++ btd6bot/bot/_farming.py
@@ -44,7 +44,7 @@ def select_rewardmap() -> str:
     kb_mouse.click(get_click('menu', 'collection_bonusrewards'))
     time.sleep(0.75)
     cprint('Selecting map with bonus rewards...')
-    failurelimit: int = 5
+    failurelimit: int = 100
     for check in range(failurelimit):
         for mapname in _MAPNAMES:
             if not strong_delta_check(mapname, get_text('menu', 'map_namebotleft'), OCR_READER):
