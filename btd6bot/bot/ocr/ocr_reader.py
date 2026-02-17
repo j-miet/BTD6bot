51e8cedee9b18c3cac94481243b379bc9333998f
j-miet <191247923+j-miet@users.noreply.github.com>
2026-02-17 18:21:14 +0200

    Add filter to mute pytorch pin_memory warning

--- btd6bot/bot/ocr/ocr_reader.py
+++ btd6bot/bot/ocr/ocr_reader.py
@@ -1,15 +1,24 @@
 """Creates a new easyocr reader.
 
-Reader takes a bit to load up. Only a single one is necessary so this loading process happens only once.
-
-No mypy type hints available for easyocr so either 'Reader' or 'Any' is used with Reader objects.
+Reader might take a bit to load up if you have older pc. Only a single one is necessary so this loading process happens 
+only once each session.
 """
+import warnings
 
 import easyocr # type: ignore
 
-from bot.bot_vars import BotVars
+from bot import _maindata
 
-OCR_READER: easyocr.Reader = easyocr.Reader(['en'], gpu=BotVars.use_gpu, verbose=False, quantize=False)
+# PyTorch gives the following warning if no CUDA support is detected:
+# OSError: [WinError 1114] A dynamic link library (DLL) initialization routine failed.
+# This message is just a warning message and it does not affect ocr operations. Following filter will mute this alert
+# and prevents it from getting displayed in the console window.
+warnings.filterwarnings("ignore", message=".*pin_memory.*")
+
+OCR_READER: easyocr.Reader = easyocr.Reader(lang_list=['en'], 
+                                            gpu=_maindata.maindata["bot_vars"]["use_gpu"], 
+                                            verbose=False, 
+                                            quantize=False)
 """Easyocr reader for english language text with gpu support disabled.
 
 On windows platforms, you can set quantize=True to test if it improves ocr accuracy.
