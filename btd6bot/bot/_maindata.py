c79d2c5f64d9a1b16fe32c35c14c343ce5c9f908
j-miet <191247923+j-miet@users.noreply.github.com>
2026-04-14 17:58:39 +0300

    Add plan debugging support

--- btd6bot/bot/_maindata.py
+++ btd6bot/bot/_maindata.py
@@ -1,10 +1,26 @@
 """Data flow of files"""
 
+from enum import Enum
 import json
 from pathlib import Path
 from typing import Any
 
 from customprint import cprint
+
+
+class Debug(Enum):
+    """Enum class for debugging modes"""
+
+    NONE = 0
+    """No debugging, default state"""
+    POS_STATIC = 1
+    """static position check, assumes tower positions don't change between rounds"""
+    POS_DYNAMIC = 2
+    """dynamic position check, towers can move between rounds. Much slower than static checking.
+    """
+    SETUP_STATIC_STATE = 3
+    """Executes all commands starting from a specific round and assumes static positioning of towers."""
+
 
 maindata: dict[str, Any] = {}
 """Central data storage for btd6bot during runtime."""
@@ -76,11 +92,13 @@ def init_maindata(maindata_dict: dict[str, Any] = maindata) -> bool:
         "defeat_status": False,
         "skip_roundcheck": False,
     }
-    temp_dict["debug"] = {
-        # NOT IMPLEMENTED: debug mode for testing if static tower positions are valid
-        "test_positions": False,
-        # NOT IMPLEMENTED: test tower positions (even round-based e.g. Geared) and costs (TODO add round income file)
-        "test_full": False,
+    temp_dict["debug"] = {  # preferably modify these value only via scripts
+        "mode": Debug.NONE,  # TODO implement POS_DYNAMIC
+        "time_limit": 5,
+        # values below are only for SETUP_STATIC_STATE
+        "debug_ignore": False,  # used for ignoring round forward and time-related commands
+        "state_startround": -1,
+        "hero_startlevel": -1,
     }
 
     if failed:
@@ -98,6 +116,66 @@ def write_botvars(new_vars: dict[str, Any]) -> None:
 def write_ocr_upgrades(new_ocrvals: dict[str, Any]) -> None:
     with open(Path(__file__).parent.parent / "Files" / "upgrades_current.json", "w") as f:
         json.dump(new_ocrvals, f, indent=2)
+
+
+def debug_pos() -> bool:
+    if maindata["debug"]["mode"] in {Debug.POS_STATIC, Debug.POS_DYNAMIC}:
+        return True
+    return False
+
+
+def debug_all() -> bool:
+    if maindata["debug"]["mode"] in {
+        Debug.POS_STATIC,
+        Debug.POS_DYNAMIC,
+        Debug.SETUP_STATIC_STATE,
+    }:
+        return True
+    return False
+
+
+def debug_get_ignore_flag() -> bool:
+    ignored: bool = maindata["debug"]["debug_ignore"]
+    return ignored
+
+
+def debug_set_ignore_flag(val: bool) -> None:
+    maindata["debug"]["debug_ignore"] = val
+
+
+def debug_get_timelimit() -> float:
+    limit: float = maindata["debug"]["time_limit"]
+    return limit
+
+
+def debug_get_mode() -> Debug:
+    mode: Debug = maindata["debug"]["mode"]
+    return mode
+
+
+def debug_set_mode(val: Debug) -> None:
+    """Sets debugging mode.
+
+    Debug.NONE = disable debugging (default)
+
+    Debug.POS_STATIC = static tower position check, all commands except tower placement & selling are ignored
+
+    Debug.SETUP_STATIC_STATE = static tower positions, run all commands each round to setup specific round state. Then
+        continue plan normally from stated round. Round must be updated u
+
+    If you change mode to anything else than Debug.NONE, it will stay that way unless you call
+    debug_set_mode(Debug.NONE) again
+    """
+    maindata["debug"]["mode"] = val
+
+
+def debug_get_startround() -> int:
+    start: int = maindata["debug"]["state_startround"]
+    return start
+
+
+def debug_set_startround(val: int) -> None:
+    maindata["debug"]["state_startround"] = val
 
 
 init_maindata()  # first init when program starts
