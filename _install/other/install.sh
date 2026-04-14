16dd93a32374c353a562b4e717cf4d0d67eea792
j-miet <191247923+j-miet@users.noreply.github.com>
2026-04-14 18:18:54 +0300

    Scripts: add debug_plan and include them in install

--- _install/other/install.sh
+++ _install/other/install.sh
@@ -9,6 +9,7 @@ cp ./_install/other/_copy/show_coordinates.sh ./tools/show_coordinates/run.sh
 cp ./_install/other/_copy/move_mouse.sh ./tools/move_mouse/run.sh
 cp ./_install/other/_copy/image_scaler.sh ./tools/image_scaler/run.sh
 cp ./_install/other/_copy/command_tracker.sh ./tools/command_tracker/run.sh
+cp ./_install/other/_copy/debug_plan.sh ./tools/debug_plan/run.sh
 python3 -m venv ./.venv
 if [[ "$OSTYPE" == "msys" || "$OSTYPE" == "cygwin" ]]; then
 	source ./.venv/Scripts/activate
