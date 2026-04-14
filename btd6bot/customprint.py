0bafc1431d5d1a41f4cd211db1e93a557d247458
j-miet <191247923+j-miet@users.noreply.github.com>
2026-04-14 18:43:59 +0300

    Add debug print

--- btd6bot/customprint.py
+++ btd6bot/customprint.py
@@ -3,22 +3,38 @@ from typing import Any
 
 from bot import _maindata
 
-def cprint(*values: Any, 
-           sep: str = " ", 
-           end: str = "\n", 
-           file: str | None = None, 
-           flush: bool = False) -> None:
+
+def cprint(*values: Any, sep: str = " ", end: str = "\n", file: str | None = None, flush: bool = False) -> None:
     """Custom print"""
     if _maindata.maindata["bot_vars"]["logging"]:
-        with open(Path(__file__).parent.parent/"Logs.txt", 'a') as logfile:
+        with open(Path(__file__).parent.parent / "Logs.txt", "a") as logfile:
             log_value = sep.join(map(str, values)) + end
             logfile.write(log_value)
-    print(*values, sep=sep, end=end, file=file, flush=flush) # type: ignore[arg-type]
+    print(*values, sep=sep, end=end, file=file, flush=flush)  # type: ignore[arg-type]
+
 
 def cinput(prompt: str = "") -> str:
     """Custom input"""
     value = input(prompt)
     if _maindata.maindata["bot_vars"]["logging"]:
-        with open(Path(__file__).parent.parent/"Logs.txt", 'a') as logfile:
-            logfile.write(str(prompt)+value+'\n')
+        with open(Path(__file__).parent.parent / "Logs.txt", "a") as logfile:
+            logfile.write(str(prompt) + value + "\n")
+    return value
+
+
+def dprint(*values: Any, sep: str = " ", end: str = "\n", file: str | None = None, flush: bool = False) -> None:
+    """Debug print"""
+    if _maindata.debug_get_mode() != _maindata.Debug.NONE:
+        with open(Path(__file__).parent.parent / "DebugLogs.txt", "a") as logfile:
+            log_value = sep.join(map(str, values)) + end
+            logfile.write(log_value)
+    print(*values, sep=sep, end=end, file=file, flush=flush)  # type: ignore[arg-type]
+
+
+def dinput(prompt: str = "") -> str:
+    """Debug input"""
+    value = input(prompt)
+    if _maindata.debug_get_mode() != _maindata.Debug.NONE:
+        with open(Path(__file__).parent.parent / "DebugLogs.txt", "a") as logfile:
+            logfile.write(str(prompt) + value + "\n")
     return value
